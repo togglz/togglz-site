035c9796a7ff25dfdc6248f1b7d85ea0246fb83d
Christian Kaltepoth <christian@kaltepoth.de>
2012-04-19 19:44:55 +0200

    Added Google Analytics

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -1,6 +1,5 @@
 
 Awestruct::Extensions::Pipeline.new do
-  # extension Awestruct::Extensions::Posts.new( '/news' ) 
-  # extension Awestruct::Extensions::Indexifier.new
+  helper Awestruct::Extensions::GoogleAnalytics
 end
 
