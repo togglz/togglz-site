f7607e68b57a50a320ed8c3101db8aa449278b4c
Christian Kaltepoth <christian@kaltepoth.de>
2017-03-02 17:23:18 +0100

    Removed old scripts

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Cleaning up..."
-rm -rf _site
-echo "Building site..."
-awestruct -P production
-echo "DONE!"
-
--- deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-s3cmd sync _site/ s3://www.togglz.org
-
